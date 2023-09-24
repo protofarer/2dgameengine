023160c6f625be3ccab09da629495e320f69fdec
Kenny Baron <kennybaron@fastmail.com>
2023-09-24 01:13:32 -0400

    wip registry

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,10 +1,11 @@
 #include "ECS.h"
+#include "../Logger/Logger.h"
+#include <algorithm>
 
 void System::AddEntityToSystem(Entity entity) {
 	entities.push_back(entity);
 }
 
-#include <algorithm>
 void System::RemoveEntityFromSystem(Entity entity) {
 	entities.erase(std::remove_if(entities.begin(), entities.end(), [&entity](Entity other) {
 		return entity.GetId() == other.GetId();
@@ -19,6 +20,51 @@ const Signature& System::GetComponentSignature() const {
 	return componentSignature;
 }
 
-int Entity::GetId() const {
-	return id;
+int Entity::GetId() const { return id; }
+
+Entity Registry::CreateEntity() {
+	int entityId;
+	entityId = numEntities++;
+	Entity entity(entityId);
+	entitiesToBeAdded.insert(entity);
+
+	Logger::Log("Entity created with id = " + std::to_string(entityId));
+
+	return entity;
+}
+
+template <typename TComponent, typename ...TArgs>
+void Registry::AddComponent(Entity entity, TArgs&& ...args) {
+	const int componentId = Component<TComponent>::GetId();
+	const int entityId = entity.GetId();
+
+	if (componentId >= componentPools.size()) {
+		componentPools.resize(componentId + 1, nullptr); // putting nothing there during resize, thus nullptr
+	}
+
+	if (!componentPools[componentId]) {
+		Pool<TComponent>* newComponentPool = new Pool<TComponent>();
+		componentPools[componentId] = newComponentPool;
+	}
+
+	// ? is type cast needed?
+	Pool<TComponent>* componentPool = Pool<TComponent>(componentPools[componentId]);
+
+	if (entityId >= componentPool->GetSize()) {
+		componentPool->Resize(numEntities);
+	}
+
+	// forward args to constructor
+	TComponent newComponent(std::forward<TArgs>(args)...);
+
+	// remember ea Component is strictly data related to entity
+	component->Set(entityId, newComponent);
+
+	// update the entity's component signature for the added component
+	entityComponentSignatures[entityId].set(componentId);
+}
+
+void Registry::Update() {
+	// TODO add entities that are waiting in queue
+	// TODO remove - " -
 }
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -2,22 +2,97 @@
 
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
+#include <set>
 
+///////////////////////////////////////////////////////////////
+// ENTITY
+///////////////////////////////////////////////////////////////
 class Entity {
 	private:
 		int id;
 	public:
 		Entity(int id): id(id) {};
+		Entity(Entity& entity) = default;
 		int GetId() const; // declare doesnt modify members/internals of class
 
+		Entity& operator =(const Entity& other) = default;
 		bool operator ==(const Entity& other) const { return id == other.id; };
 		bool operator !=(const Entity& other) const { return id != other.id; };
 		bool operator >(const Entity& other) const { return id > other.id; };
 		bool operator <(const Entity& other) const { return id < other.id; };
 };
 
-class Registry {
+///////////////////////////////////////////////////////////////
+// POOL
+// * so that Registry doesnt have to specify type of Pool (wrap it in an abstract class without a type)
+///////////////////////////////////////////////////////////////
+class IPool {
+	public:
+		virtual ~IPool() {} // forces class to be abstract
+};
 
+template <typename T>
+class Pool: public IPool {
+	private:
+		std::vector<T> data;
+
+	public:
+		Pool(int size = 100) { data.resize(size); }
+		virtual ~Pool() = default;
+
+		bool isEmpty() const { return data.empty(); }
+		int GetSize() const { return data.size(); }
+		void Resize(int n) { data.resize(n); }
+		vod Add(T object) { data.push_back(object); }
+		void Set(int index, T object) { data[index] = obect; }
+		T& Get(int index) const { return static_cast<T>(data[index]); }
+		T& operator [](unsigned int index) const { return data[index]; }
+};
+
+///////////////////////////////////////////////////////////////
+// REGISTRY
+// Manages creatiuon and destruction of entities, add systems and components
+///////////////////////////////////////////////////////////////
+class Registry {
+	public:
+		Registry() = default;
+
+		void Update();
+
+		Entity CreateEntity();
+
+		void KillEntity();
+
+		template <typename TComponent, typename ...TArgs> 
+		void AddComponent(Entity entity, TArgs&& ...args);
+
+		void AddEntityToSystem(Entity entity);
+
+		// RemoveComponent(Entity entity)
+		// HasComponent(Entity entity)
+		// GetComponent(Entity entity)
+		//
+		// AddSystem()
+		// RemoveSystem()
+		// HasSystem()
+		// GetSystem()
+	private:
+		int numEntities = 0;
+		std::set<Entity> entitiesToBeAdded; // await till next registry update
+		std::set<Entity> entitiesToBeKilled; // await till next registry update
+
+		// Ea pool contains all the data for a certain component type
+		// [Vector index = component type id]
+		// [Pool index = entity id]
+		std::vector<IPool*> componentPools;
+
+		// Vector of component sigs per entity, saying which comp is turned on for ea entity
+		// [Vector index = entity id]
+		std::vector<Signature> entityComponentSignatures;
+
+		std::unordered_map<std::type_index, System*> systems;
 };
 
 const unsigned int MAX_COMPONENTS = 32;
